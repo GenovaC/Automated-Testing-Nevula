438fc6bd0d3f4e3f0ea4262044df3cedf2b0b96a
Génova Castillo <37961016+GenovaC@users.noreply.github.com>
2019-10-04 17:25:45 -0400

    revisar dataTable

--- test/3_gestion_cuentas.js
+++ test/3_gestion_cuentas.js
@@ -1,13 +1,16 @@
+var _ = require('lodash');
 require('chromedriver');
 const assert = require('assert');
 const {Builder, Key, By, until} = require('selenium-webdriver');
 
 
-describe('Verify login with incorrect credentials', function () {
+describe('Verify Clients DataTable', function () {
     const WAIT_TIME = 3000;
     let driver;
     let realUser = "gcastillo";
     let realPassword = "Abc.123";
+    let statusClient = ["Habilitado", "Deshabilitado"];    
+    let typeClient = ["Particular", "Comercial", "Corporativo"];
 
     before(async function() {
         driver = await new Builder().forBrowser('chrome').build();
@@ -38,7 +41,7 @@ describe('Verify login with incorrect credentials', function () {
         });       
     });
 
-    it('Access to Accounts gestion', async function() {
+    it('Access to Clients gestion', async function() {
         //Presionar el menú de Gestión de Cuentas, accediendo a él mediante el "selector" que provee el inspector web
         let accountsMenu = await driver.findElements({css: 'body > div.wrapper.ng-scope > div:nth-child(2) > aside > section > ul > li:nth-child(2)'});
         await accountsMenu[0].click();
@@ -48,15 +51,86 @@ describe('Verify login with incorrect credentials', function () {
         accountSubmenu[0].click();
         await driver.sleep(WAIT_TIME);
 
-        await driver.getCurrentUrl().then(function (value) {            
+        /*await driver.getCurrentUrl().then(function (value) {            
             try {
                 assert.strictEqual(value, 'https://192.168.3.36/management/clients', 'La sesión no se encuentra en la ruta adecuada');   
                 console.log("Ubicación actual: "+value);   
             } catch (err) {
                 console.log('Evento Inesperado: '+err);  
             }
-        });   
+        });   */
+
+    });
+
+    it('Access to Client', async function() {
+        let clientsTable = await driver.findElement(By.xpath('//*[@id="dataTableClients"]/tbody')); 
+    	//Obtener las filas de la tabla
+    	let rows_table = await clientsTable.findElements(By.tagName("tr"));
+    	//Calcular el número de filas de la tabla
+        let rows_count = rows_table.length;
         
+    	//Loop will execute till the last row of table.
+    	for (var i = 0; i < rows_count; i++) {
+    	    //Recorrer las columnas de una fila específica 
+            let  Columns_row = await rows_table[i].findElements(By.tagName("td"));
+
+    	    //Calcular número de columnas/celdas. Se le resta uno para descontar la columna con botones
+            let columns_count = Columns_row.length;    
+            console.log("-------------------------------------------------- ");         
+            console.log("número de columnas en la fila " + i + " son: " + columns_count);
+            
+    	    //Loop will execute till the last cell of that specific row.
+    	    for (var column = 0; column < columns_count; column++) {
+
+                switch (column) {
+                    case 0:
+                        Columns_row[column].getText().then(function (value) {            
+                        try { 
+                            console.log("NOMBRE del cliente en la FILA " + i + ": " +value);   
+                        } catch (err) {
+                            console.log('Error en el NOMBRE del cliente de la fila '+i+' paginando cada 10 registros: '+err);  
+                        }
+                        });
+                        break;
+
+                    case 1:
+                        Columns_row[column].getText().then(function (value) {     
+                            // regex con el que revisan los emails de nevula /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i        
+                        try { 
+                            console.log("EMAIL del cliente en la FILA " + i + ": " +value);    
+                        } catch (err) {
+                            console.log('Error en el CLIENTE del cliente de la fila '+i+' paginando cada 10 registros: '+err);  
+                        }
+                        });
+                        break;
+
+                    case 2:
+                        Columns_row[column].getText().then(function (value) {            
+                        try { 
+                            if (!_.includes(typeClient, value)){
+                                console.log("TIPO del cliente en la FILA " + i + ": " +value); 
+                            }
+                        } catch (err) {
+                            console.log('Error en el TIPO del cliente de la fila '+i+' paginando cada 10 registros: '+err);  
+                        }
+                        });
+                        break;
+
+                    case 3:
+                        Columns_row[column].getText().then(function (value) {            
+                        try {   
+                            if (!_.includes(statusClient, value)){
+                                console.log("ESTADO del cliente en la FILA " + i + ": " +value); 
+                            }
+                        } catch (err) {
+                            console.log('Error en el ESTADO del cliente de la fila '+i+' paginando cada 10 registros: '+err);    
+                        }
+                        });
+                        break;
+                }
+    	    }
+    	    
+        }
     });
 
 })
